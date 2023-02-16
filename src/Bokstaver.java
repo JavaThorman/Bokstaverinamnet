28e063c25b413dd3294b7c06c072b3bf855224ed
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-16 12:39:58 +0100

    changes

--- src/Bokstaver.java
+++ src/Bokstaver.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Bokstaver {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.println("Hello! Please enter your name!");
+        name = input.next();
+        int length = 0;
+        for (int i = 0; i < name.length(); i++) {
+            length++;
+        }
+        System.out.println("You name is "+ length + " long!");
+    }
 }
